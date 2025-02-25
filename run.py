1116120c55f69d86c115669d86fffb5825a16e88
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-02-25 23:29:24 +0000

    Add: avg stock calculation and add to stock sheet

--- run.py
+++ run.py
@@ -90,6 +90,21 @@ def get_last_5_entries():
         columns.append(column[-5:])
     
     return columns
+    avg_stock_data = []
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    avg_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * .1
+        avg_stock_data.append(round(stock_num + average))
+
+    return avg_stock_data
+
 def main():
     """
     Run all program functions
@@ -99,6 +114,9 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-get_last_5_entries()
+main()
