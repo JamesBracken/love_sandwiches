b15d0c17e653deb0e80b4eeab634c680a3a138d8
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-02-25 23:12:56 +0000

    Add: data collection for last 5 trading days

--- run.py
+++ run.py
@@ -77,7 +77,19 @@ def update_worksheet(data, sheet):
     worksheet_to_update.append_row(data)
     print(f"{sheet} worksheet updated successfully.\n")
 
-
+def get_last_5_entries():
+    """
+    Collects collumns of data from sales worksheets, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 def main():
     """
     Run all program functions
@@ -88,4 +100,5 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+get_last_5_entries()
